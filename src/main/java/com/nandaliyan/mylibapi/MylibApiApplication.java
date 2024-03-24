b9afa220f4ac075a9b8747bf2d9a6a06ea5c9917
Rais A. <nandaliyan.rais@gmail.com>
2024-03-24 21:08:57 +0700

    Initial commit.

---
+++ src/main/java/com/nandaliyan/mylibapi/MylibApiApplication.java
@@ -0,0 +1,13 @@
+package com.nandaliyan.mylibapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylibApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylibApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nandaliyan/mylibapi/MylibApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandaliyan.mylibapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
